11ef0a23d6ab7cf64a2baa3952410f9482a82531
Marinegrn <marinegarein@outlook.fr>
2025-07-25 17:44:54 +0200

    WIP : TEST db

--- backend/server/src/index.ts
+++ backend/server/src/index.ts
@@ -1,5 +1,5 @@
 import './config/dotenv'; // doit arriver avant les autres imports
-// console.log('DATABASE_URL:', process.env.DATABASE_URL); // revert / TEST -> DOUBLE OK ! 
+console.log('DATABASE_URL:', process.env.DATABASE_URL); // revert / TEST -> DOUBLE OK ! 
 
 import express from 'express';
 import cors from 'cors';
